2a8331a2560953ab56b5328b0f960cb054cad08e
Nick Levkovich <nicklevkovich@gmail.com>
2018-10-10 01:09:53 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,37 +1,31 @@
 module.exports = function makeExchange(currency) {
     let error = {error: "You are rich, my friend! We don't have so much coins for exchange"};
-    if (currency == 0) return ({})
-    else if (currency < 0) return ({})
+    if ((currency == 0)||(currency < 0)) return ({})
     else if (currency>10000) return(error)
-    else if ((currency>0)&&(currency<=10000)) {
+    else {
     let output = {};
     while (currency){
-        if (currency>=50) {
-            var Half = Math.floor(currency/50);
-            currency = (currency-Half*50);
-            output["H"] = Half;
+        if (currency >= 50) {
+            output.H = Math.floor(currency/50);
+            currency -= output.H*50;
         }
         if (currency>=25) {
-            var Quart = Math.floor(currency/25);
-            currency = (currency-Quart*25);
-            output["Q"] = Quart;
+            output.Q = Math.floor(currency/25);
+            currency -= output.Q*25;
         }
         if (currency>=10) {
-            var Dim = Math.floor(currency/10);
-            currency = (currency-Dim*10);
-            output["D"] = Dim;
+            output.D = Math.floor(currency/10);
+            currency -= output.D*10;
         }
         if (currency>=5) {
-            var Nick = Math.floor(currency/5);
-            currency = (currency-Nick*5);
-            output["N"] = Nick;
+            output.N = Math.floor(currency/5);
+            currency -= output.N*5;
         }
         if (currency>=1) {
-            output["P"] = currency;
+            output.P = currency;
         }
         currency = 0;
     }
-    
     return (output);
-};
+    };
 }
